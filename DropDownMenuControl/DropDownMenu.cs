c7ac7b176f6bd6ec14c8870aad470fcead6bcf5b
Gheorghe Ganganu <gheorghe.ganganu@commatix.de>
2025-03-13 17:09:48 +0100

    DropDownControl eingefügt. TODO// TemplateBindings für das Control

---
+++ DropDownMenuControl/DropDownMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DropDownMenuControl
+{
+    public class DropDownMenu : ContentControl
+    {
+        static DropDownMenu()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(DropDownMenu), new FrameworkPropertyMetadata(typeof(DropDownMenu)));
+        }
+
+
+
+        public bool IsOpen
+        {
+            get { return (bool)GetValue(IsOpenProperty); }
+            set { SetValue(IsOpenProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for IsOpen.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsOpenProperty =
+            DependencyProperty.Register("IsOpen", typeof(bool), typeof(DropDownMenu), new PropertyMetadata(false));
+    }
+}
