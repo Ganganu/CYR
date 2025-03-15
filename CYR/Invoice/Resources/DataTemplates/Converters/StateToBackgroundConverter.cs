36496cc03f9eafe0010daacd235bf85a6bb66122
Ganganu <ganganujohnny@yahoo.com>
2025-03-15 16:05:45 +0100

    Farbe und BorderBrush des Borders wird erfolgreich geupdated.

---
+++ CYR/Invoice/Resources/DataTemplates/Converters/StateToBackgroundConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using CYR.Invoice.InvoiceModels;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CYR.Invoice.Resources.DataTemplates.Converters
+{
+    public class StateToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var state = (InvoiceState)value;
+            return state == InvoiceState.Open ? new SolidColorBrush(Color.FromRgb(235, 249, 235)) :
+                                               new SolidColorBrush(Color.FromRgb(253, 235, 235));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CYR/Invoice/Resources/DataTemplates/Converters/StateToBorderBrushConverter.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using CYR.Invoice.InvoiceModels;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CYR.Invoice.Resources.DataTemplates.Converters
+{
+    public class StateToBorderBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var state = (InvoiceState)value;
+            return state == InvoiceState.Open ? new SolidColorBrush(Colors.Green) :
+                                               new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
