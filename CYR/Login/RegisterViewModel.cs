fb5c307b311899eb008ecf127fee8650f3589471
Ganganu <ganganujohnny@yahoo.com>
2025-08-08 17:24:36 +0200

    Register -> Beim Erfolg oder Fehler wird eine Meldung angezeigt

--- CYR/Login/RegisterViewModel.cs
+++ CYR/Login/RegisterViewModel.cs
@@ -1,7 +1,12 @@
 ﻿using System.Collections.ObjectModel;
+using System.Data.SQLite;
+using System.Linq.Expressions;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using CYR.Core;
+using CYR.Dialog;
 using CYR.User;
 
 namespace CYR.Login;
@@ -9,11 +14,14 @@ namespace CYR.Login;
 public partial class RegisterViewModel : ObservableRecipient
 {
     private readonly UserRepository _userRepository;
-    public RegisterViewModel(UserRepository userRepository)
+    private readonly IDialogService _dialogService;
+    private string? _dialogResponse;
+    public RegisterViewModel(UserRepository userRepository, IDialogService dialogService)
     {
         Roles = ["Admin", "User"];
         ErrorTexts = [];
         _userRepository = userRepository;
+        _dialogService = dialogService;
     }
 
     [ObservableProperty]
@@ -36,18 +44,34 @@ public partial class RegisterViewModel : ObservableRecipient
     [RelayCommand(CanExecute = nameof(CanCreateUser))]
     private async Task CreateUser()
     {
+        var succes = false;
         ErrorTexts?.Clear();
-        if (Password != ConfirmPassword) ErrorTexts?.Add(@"Die Eingabe stimmt nicht überein mit dem eingegebenen Kennwort.");
+        if (Password != ConfirmPassword) ErrorTexts?.Add(@"Die Passwörter stimmen nicht überein. Bitte wiederholen Sie Ihre Eingabe.");
         if (SelectedRole is null || string.IsNullOrEmpty(SelectedRole)) ErrorTexts?.Add("Wählen sie eine Role."); 
         ErrorText = ErrorTexts?.Count > 0 ? string.Join(Environment.NewLine, ErrorTexts) : string.Empty;
 
         if (ErrorTexts?.Count == 0)
         {
+            int result;
             User.User registerModel = new User.User();
             registerModel.Username = Username;
             registerModel.Password = Password;
             registerModel.Role= SelectedRole;
-            var response = await _userRepository.InsertAsync(registerModel);
+            try
+            {
+                result = await _userRepository.InsertAsync(registerModel);
+            }
+            catch (SQLiteException ex)
+            {
+                SQLiteErrorCodes errorCode = (SQLiteErrorCodes)Convert.ToInt32(ex.ErrorCode);
+                ShowNotificationDialog("Konto erstellen", @$"Problem aufgetreten beim Erstellen des neuen Kontos! {Environment.NewLine}Fehler:{errorCode}", "Ok", "Error", Visibility.Collapsed, "");
+                return;
+            }
+            succes = result > 0;
+        }
+        if (succes)
+        {
+            ShowNotificationDialog("Konto erstellen", "Konto erfolgreich erstellt.", "Ok", "Check", Visibility.Collapsed,"");
         }
     }
 
@@ -77,6 +101,27 @@ public partial class RegisterViewModel : ObservableRecipient
     private void NavigateBack()
     {
         Messenger.Send<NavigateToLoginMessage>();
+    }
+
+    private void ShowNotificationDialog(string title,
+        string message,
+        string cancelButtonText,
+        string icon,
+        Visibility okButtonVisibility, string okButtonText)
+    {
+        _dialogService.ShowDialog(result =>
+        {
+            _dialogResponse = result;
+        },
+        new Dictionary<Expression<Func<NotificationViewModel, object>>, object>
+        {
+            { vm => vm.Title, title },
+            { vm => vm.Message,  message},
+            { vm => vm.CancelButtonText, cancelButtonText },
+            { vm => vm.Icon,icon },
+            { vm => vm.OkButtonText, okButtonText },
+            { vm => vm.IsOkVisible, okButtonVisibility}
+        });
     }
 }
 
